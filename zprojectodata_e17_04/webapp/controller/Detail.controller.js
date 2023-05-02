4bf3156a344bb671c8586f1315980f20d6d47dfb
yjyjylee <yoonji2006@gmail.com>
2023-05-02 18:32:40 +0900

    05.02

--- zprojectodata_e17_04/webapp/controller/Detail.controller.js
+++ zprojectodata_e17_04/webapp/controller/Detail.controller.js
@@ -9,14 +9,31 @@ sap.ui.define([
 
         return Controller.extend("zprojectodatae1704.controller.Detail", {
             onInit: function () {
-                // var oRouter = this.getOwnerComponent().getRouter();
-                // oRouter.getRoute("RouteDetail").attachPatternMatched(this._onPatternMatched,this)
-                // //RouteDetail에 해당pattern이 맞으면 onPatternMatched라는 함수를 실행하겠다
-            }, 
-        
-            onNavButtonPress: function() {
                 var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.navTo("RouteMain", {}, true) ;
+                oRouter.getRoute("RouteDetail").attachPatternMatched(this._onPatternMatched, this)
+                // //RouteDetail에 해당pattern이 맞으면 onPatternMatched라는 함수를 실행하겠다
+            },
+
+            _onPatternMatched: function (oEvent) {
+                // oEvent.getParameters().arguments; 이거랑 똑같음.
+                var oArgu = oEvent.getParameter("arguments");
+                var oModel = this.getView().getModel(); //Northwind Odata Model
+                var oFilter = new sap.ui.model.Filter('OrderID','EQ',oArgu.key); // filter(필드이름, 조건, 값)
+                console.log(oArgu); // { key : 10248 }
+
+                oModel.read("/Orders", {
+                    filters: [oFilter],
+                    success:function(oReturn) {
+                        console.log(oReturn.results[0]);
+                    }
+                });
+
+                console.log("pattern Matched function");
+            },
+
+            onNavButtonPress: function () {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("RouteMain", {}, true);
                 //navTo(1,2,3)
                 // 1: Route name
                 // 2: parameters
--- zprojectodata_e17_04/webapp/controller/Main.controller.js
+++ zprojectodata_e17_04/webapp/controller/Main.controller.js
@@ -11,11 +11,48 @@ sap.ui.define([
             onInit: function () {
 
             },
-            onSelectionChange : function(oEvent) {
+            onSelectionChange: function (oEvent) {
                 var oRouter = this.getOwnerComponent().getRouter();
                 var sPath = oEvent.getParameters().listItem.getBindingContextPath(),
-                skey = this.getView().getModel().getProperty(sPath+'/OrderID');
-                oRouter.navTo("RouteDetail" , {"key": skey});
+                    skey = this.getView().getModel().getProperty(sPath + '/OrderID');
+                oRouter.navTo("RouteDetail", { "key": skey });
+            },
+            onValueHelpRequest: function () {
+                // sap.m.MessageToast.show("Input Value Help 실행!");
+                var oDialog = this.byId("CustomerDialog");
+                if (oDialog) {
+                    oDialog.open();
+                    return;
+                }
+                this.loadFragment({
+                    name: "zprojectodatae1704.view.fragment.CustomerDialog"
+                }).then(function (oDialog) {
+                    oDialog.open();
+                }, this);
+            },
+            onClose: function () {
+                var oDialog = this.byId("CustomerDialog");
+
+                oDialog.close();
+            },
+            onRowSelectionChange: function (oEvent) {
+                // var oDialog = this.byId("CustomerDialog");
+                var oDialog = oEvent.getSource().getParent();
+                var sID = oEvent.getParameters().rowContext.getProperty('CustomerID');
+                this.byId("input").setValue(sID);
+                
+
+                this.onSearch(sID);
+
+                oDialog.close();
+                // this.onClose();
+            },
+            onSearch: function(oEvent) {
+                var sID = this.byId("input").getValue();
+                let oFilter =  new sap.ui.model.Filter('CustomerID','EQ', sID);
+               
+                this.byId("idProductsTable").getBinding("items").filter(oFilter);
             }
-        }); 
+
+        });
     });
